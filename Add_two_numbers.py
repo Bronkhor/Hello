4352976c003961e5d3377a77a55d2b330e4ff2eb
Bronkhor <jan.bronkhorst@live.nl>
2026-05-07 13:21:25 +0200

    New Add_2_numbers

---
+++ Add_two_numbers.py
@@ -0,0 +1,14 @@
+def add_two_numbers(num1, num2):
+    """
+    This function takes two numbers as input and returns their sum.
+    
+    Parameters:
+    num1 (int or float): The first number to be added.
+    num2 (int or float): The second number to be added.
+    
+    Returns:
+    int or float: The sum of num1 and num2.
+    """
+    return num1 + num2
+
+print(add_two_numbers(5, 10))
